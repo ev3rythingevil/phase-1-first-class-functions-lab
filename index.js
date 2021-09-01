e82e2b5e607a20baa3268ebb1f493ae181fb2daa
Hathor Hamilton <koshknot@gmail.com>
2021-09-01 11:33:08 -0400

    commit tree 92037f5bda1ac0b988b123fa79a767a98e2c919e

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+ let drivers = array.slice(0,2);
+    return drivers;
+}
+
+const returnLastTwoDrivers = function(array){
+    let lastDrivers = array.slice(-2);
+    return lastDrivers;
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num){
+    const multiplier = (num) => num * num;
+    return multiplier;
+}
+
+function fareDoubler(fare){
+    const doubler = fare * 2;
+    return doubler;
+}
+
+function fareTripler(fare){
+    const tripler = fare * 3;
+    return tripler;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, fnc){
+      return fnc(arrayOfDrivers);
+}
